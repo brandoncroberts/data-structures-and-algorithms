850422fad630d0700d097f3e67f8180bf612daee
Brandon C. Roberts <34548660+brandoncroberts@users.noreply.github.com>
2024-11-16 09:49:54 +0000

    update test

--- problems/containsDuplicate/main_test.ts
+++ problems/containsDuplicate/main_test.ts
@@ -1,12 +1,11 @@
 import { assertEquals } from "jsr:@std/assert";
 import { containsDuplicate } from "./main.ts";
 
-Deno.test("containsDuplicate returns correct result", () => {
+Deno.test("containsDuplicate returns correct result short array", () => {
     assertEquals(containsDuplicate([1, 2, 3, 1]), true);
 });
 
-// create test that makes an array with numbers 1 to 10000 in order, and expects false
-Deno.test("containsDuplicate returns correct result", () => {
+Deno.test("containsDuplicate returns correct result long array", () => {
     const arr = Array.from({ length: 10000 }, (_, i) => i + 1);
-    assertEquals(containsDuplicate(arr), true);
+    assertEquals(containsDuplicate(arr), false);
 });
