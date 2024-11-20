e194d76bd8b8ebf4884cc661e9d8bc96e5bb872f
Brandon C. Roberts <34548660+brandoncroberts@users.noreply.github.com>
2024-11-20 10:54:23 +0100

    simplify bruteforce

--- problems/maxProfit/main.ts
+++ problems/maxProfit/main.ts
@@ -1,31 +1,57 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+// Example 2:
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+
+// Constraints:
+
+// 1 <= prices.length <= 105
+// 0 <= prices[i] <= 104
+
 export const maxProfitBruteForce = (prices: number[]): number => {
-    let maxProfit = 0;
-    let i;
-    let j;
-    for (i = 0; i < prices.length; i++) {
-        for (j = i + 1; j < prices.length; j++) {
-            if (prices[j] > prices[i] && (prices[j] - prices[i]) > maxProfit) {
-                maxProfit = prices[j] - prices[i];
-            }
-        }
+  let maxProfit = 0;
+  let i;
+  let j;
+  for (i = 0; i < prices.length; i++) {
+    for (j = i + 1; j < prices.length; j++) {
+      const difference = prices[j] - prices[i];
+      if (difference > maxProfit) {
+        maxProfit = difference;
+      }
     }
-    return maxProfit;
+  }
+  return maxProfit;
 };
 
 export const maxProfit = (prices: number[]): number => {
-    let maxProfit = 0;
-    let l = 0;
-    let r = 1;
+  let maxProfit = 0;
+  let l = 0;
+  let r = 1;
 
-    while (r < prices.length) {
-        const difference = prices[r] - prices[l];
-        if (difference > 0) {
-            maxProfit = Math.max(difference, maxProfit);
-        } else {
-            l = r;
-        }
-        r++;
+  while (r < prices.length) {
+    const difference = prices[r] - prices[l];
+    if (difference > 0) {
+      maxProfit = Math.max(difference, maxProfit);
+    } else {
+      l = r;
     }
+    r++;
+  }
 
-    return maxProfit;
+  return maxProfit;
 };
