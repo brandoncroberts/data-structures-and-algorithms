9b54715215dfff7cd92c713ef29b109253477d5b
Brandon C. Roberts <34548660+brandoncroberts@users.noreply.github.com>
2024-11-21 13:03:01 +0100

    add LinkedList Constructor

--- src/data-structures/linked-list/linked-list-constructor/main.ts
+++ src/data-structures/linked-list/linked-list-constructor/main.ts
@@ -1,1 +1,26 @@
-export const linked_list_constructor = () => {};
+type NullableNode = Node | null;
+
+export class Node {
+  value: number;
+  next: NullableNode;
+
+  constructor(value: number, next: NullableNode = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+export class LinkedList {
+  head: NullableNode = null;
+  tail: NullableNode = null;
+  length: number = 0;
+
+  constructor(value?: number) {
+    if (value !== undefined) {
+      const newNode = new Node(value);
+      this.head = newNode;
+      this.tail = newNode;
+      this.length = 1;
+    }
+  }
+}
--- src/data-structures/linked-list/linked-list-constructor/main_test.ts
+++ src/data-structures/linked-list/linked-list-constructor/main_test.ts
@@ -1,6 +1,57 @@
 import { assertEquals } from "jsr:@std/assert";
-import { linked_list_constructor } from "./main.ts";
+import { LinkedList, Node } from "./main.ts";
 
-Deno.test("linked_list_constructor returns correct result", () => {
-    assertEquals(linked_list_constructor(), 'expected result');
+Deno.test(
+  "LinkedList initializes correctly with a single node when initialized with a value",
+  () => {
+    const linkedList = new LinkedList(5);
+    const expectedLinkedList: InstanceType<typeof LinkedList> = {
+      head: new Node(5),
+      tail: new Node(5),
+      length: 1,
+    };
+
+    assertEquals(linkedList.head!.value, expectedLinkedList.head!.value);
+    assertEquals(linkedList.tail!.value, expectedLinkedList.tail!.value);
+    assertEquals(linkedList.length, expectedLinkedList.length);
+  }
+);
+
+Deno.test(
+  "LinkedList initializes correctly with an empty LinkedList when initialized without a value",
+  () => {
+    const linkedList = new LinkedList();
+    const expectedLinkedList: InstanceType<typeof LinkedList> = {
+      head: null,
+      tail: null,
+      length: 0,
+    };
+
+    assertEquals(linkedList.head, expectedLinkedList.head);
+    assertEquals(linkedList.tail, expectedLinkedList.tail);
+    assertEquals(linkedList.length, expectedLinkedList.length);
+  }
+);
+
+Deno.test("Node initializes correctly with a value and no next node", () => {
+  const node = new Node(5);
+  const expectedNode: InstanceType<typeof Node> = {
+    value: 5,
+    next: null,
+  };
+
+  assertEquals(node.value, expectedNode.value);
+  assertEquals(node.next, expectedNode.next);
+});
+
+Deno.test("Node initializes correctly with a value and a next node", () => {
+  const nextNode = new Node(10);
+  const node = new Node(5, nextNode);
+  const expectedNode: InstanceType<typeof Node> = {
+    value: 5,
+    next: nextNode,
+  };
+
+  assertEquals(node.value, expectedNode.value);
+  assertEquals(node.next, expectedNode.next);
 });
